438ae07a67331578125151ad49badafba9aa124f
Jiri Pelc <mianen@DESKTOP-0R3QH5F.localdomain>
2019-10-02 10:10:08 +0200

    Test

---
+++ Lab01/pTop.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+//#include <string.h>
+//#include <errno.h>
+//#include <limits.h>
+//#include <assert.h>
+
+
+
+//Structs
+
+//Functions
+
+/*
+ * argv 
+ * */
+int main(int argc, const char* argv[])
+{
+	printf("Hallo World\n");
+	void* p;
+	p = &p;
+
+	printf("%p, %p\n", &p, p);
+
+}
+
