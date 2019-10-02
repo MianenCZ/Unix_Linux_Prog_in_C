8ee67c37cd23470857a906244fd1b1f693b1eac2
Jiri Pelc <mianen@DESKTOP-0R3QH5F.localdomain>
2019-10-02 10:12:38 +0200

    Add another file

---
+++ Lab01/writeArgv.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+//#include <string.h>
+//#include <errno.h>
+//#include <limits.h>
+//#include <assert.h>
+
+
+
+//Structs
+
+//Functions
+
+/*
+ * argv 
+ * */
+int main(int argc, const char* argv[])
+{
+	printf("Hallo World\n");
+}
+
